54960422716d6f291c7fe9595bbca452aea47dbc
azimsaqib10@gmail.com <azimsaqib10@gmail.com>
2016-07-17 14:12:59 -0700

    Interface for students to login and sign up

--- LargeCoachingInstitute/StudentLogin.py
+++ LargeCoachingInstitute/StudentLogin.py
@@ -2,20 +2,20 @@ import json
 import getpass 
 import os  
 
+#to check whether the StudentRecords file is completely empty or not
 def checkFileEmpty():
         if os.stat("C:\Users\Saqib\Desktop\Snake\LargeCoachingInstitute\StudentRecords.txt").st_size == 0:
             print "Empty File!!!Please add members to perform these operations"
             return True
         else:
             return False
-
+#for student to login(if signed up) to view their current marks and batch details
 def login():
 	found=False
 	if checkFileEmpty()==True:              #checks whether the file is empty or not
 		return
 	username=raw_input("User Name >>>")
 	pwd=getpass.getpass("Password >>>")
-	
 	with open('C:\Users\Saqib\Desktop\Snake\LargeCoachingInstitute\StudentRecords.txt') as readFile:
 		jsonData=json.load(readFile)
         for i in range(len(jsonData)):
@@ -26,16 +26,14 @@ def login():
         if found==False:
         	print "\nNot registered or not admitted yet!!!If Registered ,then Sign Up!!!\n"
     
-
-#for the first time users.
+#for students who have been admitted 
 def signUp():
 	found=False
 	countOfTry=0
 	if checkFileEmpty()==True:              #checks whether the file is empty or not
 		return
 	name=raw_input("Name >>>")
-	phoneno=raw_input("Phone No >>>")
-	
+	phoneno=raw_input("Phone No >>>")	
 	with open('C:\Users\Saqib\Desktop\Snake\LargeCoachingInstitute\StudentRecords.txt') as readStudentFile:
 		jsonData=json.load(readStudentFile)
 		for i in range(len(jsonData)):
@@ -43,7 +41,7 @@ def signUp():
 				found=True
 				username=raw_input("UserName >>>")
 				pwd=getpass.getpass("Password >>>")
-				confirmpwd=getpass.getpass("Confirm Password >>>")
+				confirmpwd=getpas.getpass("Confirm Password >>>")
 				if pwd!=confirmpwd:
 					print "Password don't match!!!Try Again(max. 3 times)"
 					countOfTry+=1
@@ -51,15 +49,14 @@ def signUp():
 						break
 				else:
 					#Register the student username and password
-					print "\nRegistration complete\n"
+					print "\nRegistration complete!!! Now Login to view your Current Status\n"
+					os.system("pause")
 					jsonData[i]['username']=username
 					jsonData[i]['password']=pwd
-					open('C:\Users\Saqib\Desktop\Snake\LargeCoachingInstitute\StudentRecords.txt','w').write(json.dumps(jsonData,indent=4))
-					
+					open('C:\Users\Saqib\Desktop\Snake\LargeCoachingInstitute\StudentRecords.txt','w').write(json.dumps(jsonData,indent=4))	
 				break
 		if found==False:
 			print "\nNot admitted yet or data filled incorrectly or already registered\n"
-
 
 while(True):	
 	print "Press 1 to Login to view Results"
